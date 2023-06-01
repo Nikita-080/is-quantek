9ea01bd644103cda952d765804fcf2d43896e976
Никита <nikitarabov162@gmail.com>
2023-06-01 18:05:03 +0500

    cpu frequency, disk size

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,5 +1,13 @@
 from winreg import * #for processor
 import platform
+import psutil        #processor freq, disk
+
+def Round2(x):
+    i=1
+    while i<x:
+        i*=2
+    return i
+
 
 while True:
     print("Введите папку для сохранения (или n):")
@@ -14,13 +22,19 @@ while True:
 
 
     #get system info #begin
+        
     aReg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
     aKey = OpenKey(aReg, r"HARDWARE\DESCRIPTION\System\CentralProcessor\0")
     proc = QueryValueEx(aKey, 'ProcessorNameString')[0].split()
     proccreator=proc[0]
     procmodel=proc[1]+' '+proc[2]
+    procfreq=str(int(float(psutil.cpu_freq().max)/1000))
     print("Производитель процессора -",proccreator)
     print("Модель процессора        -",procmodel)
+    print("Частота процессора       -",procfreq)
+
+    disksize=Round2(psutil.disk_usage("C:/").total/pow(2,30)) #WARNING (TODO some disks)
+    print("Объем диска              -",disksize)
     
 
     #get system info #end
